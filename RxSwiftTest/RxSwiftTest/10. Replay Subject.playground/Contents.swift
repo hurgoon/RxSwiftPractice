f8253b4e7313e931a61a063e2ee506ca7a8e5c50
Bernard <hurgoon@gmail.com>
2019-03-10 21:52:31 +0900

    10. Replay Subject

---
+++ RxSwiftTest/RxSwiftTest/10. Replay Subject.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+import RxSwift
+
+enum SearchError: Error {
+    case unknown
+}
+
+
+func replay() {
+    
+    let subject = ReplaySubject<String>.create(bufferSize: 2) // 처음 한번은 모든 구독 발행
+    
+    subject.onNext("YouTube")
+    
+    let bag = DisposeBag()
+    
+    subject.subscribe { (event) in
+        print("1)", event.element ?? event)
+    }.disposed(by: bag)
+    
+    subject.onNext("Netflix")
+    
+    subject.onNext("Prime Video")
+    
+    subject.subscribe { (event) in  // 두번째 구독부터 "버퍼 사이즈(역순)"만 발행
+        print("2)", event.element ?? event)
+    }.disposed(by: bag)
+    
+//    subject.onCompleted()
+    
+    subject.onError(SearchError.unknown)
+    
+    subject.dispose() // 디스포즈를 먼저 했기 때문에 그후에 구독은 발행하지 않는다
+    
+    subject.onNext("IMDB")
+    
+    subject.subscribe { (event) in
+        print("3)", event.element ?? event)
+    }.disposed(by: bag  )
+}
+replay()
