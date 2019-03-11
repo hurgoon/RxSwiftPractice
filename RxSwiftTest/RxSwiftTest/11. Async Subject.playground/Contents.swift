0768965b2f09c0417c2a1320455e65c47154a626
Bernard <hurgoon@gmail.com>
2019-03-12 03:36:04 +0900

    update

--- RxSwiftTest/RxSwiftTest/11. Async Subject.playground/Contents.swift
+++ RxSwiftTest/RxSwiftTest/11. Async Subject.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+enum SomeError: Error {
+    case unknown
+}
+
+func async() {
+    
+    let index = AsyncSubject<Int>() // completed가 필요하며, 마지막 밸류만 발행된다
+    
+    index.onNext(0)
+    
+    let bag = DisposeBag()
+    
+    index.subscribe { (event) in
+        print("1)", event.element ?? event)
+    }.disposed(by: bag)
+    
+    index.onNext(1)
+    
+    index.subscribe { (event) in
+        print("2)", event.element ?? event)
+    }.disposed(by: bag)
+    
+    index.onNext(2)
+    index.onNext(3)
+    index.onCompleted()
+//    index.onError(SomeError.unknown) // onCompleted는 안됐지만 에러는 발행됨. // onCompleted 뒤에는 에러가 와도 발행 노.
+    
+    index.subscribe { (event) in
+        print("3)", event.element ?? event)
+    }.disposed(by: bag)
+
+}
+async()
