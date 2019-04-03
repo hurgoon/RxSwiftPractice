92c8740298cbed03a9b0836f708c0a8a969bd193
Bernard <hurgoon@gmail.com>
2019-04-04 00:27:25 +0900

    36. Replay 2

--- RxSwiftTest/RxSwiftTest/Playgrounds/36. Replay.playground/Contents.swift
+++ RxSwiftTest/RxSwiftTest/Playgrounds/36. Replay.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+extension DispatchSource {
+    class func timer(interval : Double, queue: DispatchQueue, handler: @escaping () -> Void ) -> DispatchSourceTimer {
+        
+        let timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.schedule(deadline: .now(), repeating: interval, leeway: .nanoseconds(0))
+        timer.setEventHandler(handler: handler)
+        timer.resume()
+        
+        return timer
+    }
+}
+
+let elementsPerSecond: Double = 1
+let totalElements = 5
+let bufferSize = 1
+let delay: TimeInterval = 3
+let sourceObservable = Observable<Int>.create { (observer) -> Disposable in
+    var count = 1
+    let timer = DispatchSource.timer(interval: 1/elementsPerSecond, queue: .main, handler: {
+        if count <= totalElements {
+            observer.onNext(count)
+            count += 1
+        }
+    })
+    return Disposables.create {
+        timer.suspend()
+    }
+}.replay(bufferSize)
+_ = sourceObservable.subscribe( { print("Control", $0) })
+DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+    _ = sourceObservable.subscribe({ print("Delay", $0) })
+}
+
+_ = sourceObservable.connect()
+
+
