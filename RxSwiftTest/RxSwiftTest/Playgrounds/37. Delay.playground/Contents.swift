f0f09f972d8af2791878136e5f6c22f5ff723e24
Bernard <hurgoon@gmail.com>
2019-04-06 23:31:42 +0900

    37. Delay

--- RxSwiftTest/RxSwiftTest/Playgrounds/37. Delay.playground/Contents.swift
+++ RxSwiftTest/RxSwiftTest/Playgrounds/37. Delay.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+extension DispatchSource {
+    class func timer(interval : Double, queue: DispatchQueue, handler: @escaping () -> Void ) -> DispatchSourceTimer {
+        
+        let timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.schedule(deadline: .now(), repeating: interval, leeway: .nanoseconds(0))
+        timer.setEventHandler(handler: handler)
+        timer.resume()
+        
+        return timer
+    }
+}
+
+let elementsPerSecond: Double = 1
+let delay: TimeInterval = 1.5
+let subject = PublishSubject<Int>()
+
+_ = subject.subscribe({ print("Control: ", $0) })
+_ = subject
+//    .delaySubscription(RxTimeInterval(delay), scheduler: MainScheduler.instance)
+    .delay(RxTimeInterval(delay), scheduler: MainScheduler.instance)
+    .subscribe({ print("Delay Subscription", $0) })
+
+var count = 0
+
+let timer = DispatchSource.timer(interval: 1/elementsPerSecond, queue: .main) {
+    subject.onNext(count)
+    count += 1
+}
+
